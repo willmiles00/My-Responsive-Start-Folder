7cce2ac91ad6c8f3b5f06fa9b1d34152fd0b3b3d
Will <113927655+willmiles00@users.noreply.github.com>
2023-09-25 10:54:18 -0600

    joey joe joe shabadoo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+function toggleMenu(){
+document.getElementById("primaryNav").classList.toggle("open")
+}
+
+const hamburgerBtn = document.getElementById("hamburgerBtn")
+
+hamburgerBtn.onclick = toggleMenu
+
 // footer current year
 const currentYear = new Date()
 document.querySelector('#year').textContent = currentYear.getFullYear()
+
