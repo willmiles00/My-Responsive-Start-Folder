f02c1b47a795a7424757fe49fa3a806c8ef854bd
Will <113927655+willmiles00@users.noreply.github.com>
2023-11-27 21:02:54 -0700

    bbbb

---
+++ js/reviews.js
@@ -0,0 +1,27 @@
+export const reviews = [
+    {
+        "name": "David Smith",
+        "stars": 1,
+        "text": "I've been taking my cars to this mechanic for years, and I've never been anything but satisfied with their service. The mechanics are always friendly and knowledgeable, and they've always been able to diagnose and fix my car problems quickly and efficiently. I highly recommend this mechanic to anyone looking for a reliable and honest car mechanic."
+    },
+    {
+        "name": "Emily Wiliams",
+        "stars": 5,
+        "text": "I was in a bind when my car broke down on the side of the road. I called this mechanic, and they were able to send a tow truck to me right away. The mechanic was able to diagnose the problem quickly, and he had my car up and running in no time. I was so grateful for their help!"
+    },
+    {
+        "name": "Charlie Johnson",
+        "stars": 4,
+        "text": "I'm always hesitant to take my car to a mechanic because I'm afraid of getting ripped off. But this mechanic has changed my mind. They're always up-front about their prices, and they never try to sell me anything I don't need. I trust them completely with my car."
+    },
+    {
+        "name": "Alice Brown",
+        "stars": 3,
+        "text": "My car has been making a strange noise for weeks, and I couldn't figure out what was wrong with it. I took it to this mechanic, and they were able to identify the problem right away. It turned out to be a loose belt, and it was an easy fix. I'm so glad I took my car to this mechanic!"
+    },
+    {
+        "name": "Bob Crane",
+        "stars": 3,
+        "text": "I'm a busy professional, and I don't have time to deal with car problems. That's why I'm so glad I found this mechanic. They're always willing to work with my schedule, and they always get the job done right. I highly recommend this mechanic to anyone looking for a hassle-free car repair experience."
+    },
+]
--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
+import { reviews } from "./reviews.js"
+console.log(reviews)
+
+//creates a card
+const myTarget = document.querySelector("#cards")
+
+for (let x= 0; x < reviews.length; x++){
+
+    //creates a new section
+    const mySection = document.createElement('section')
+    const myName = document.createElement('h3')
+    myName.textContent = reviews[x].name
+
+    const myReview = document.createElement('p')
+    myReview.textContent = reviews[x].text
+
+    const myRating = document.createElement('div')
+
+   
+
+    for (let step = 0; step < reviews[x].stars; step++) {
+        const starIndicator = document.createElement('img')
+        starIndicator.src = './SVG/star-solid.svg'
+        myRating.appendChild(starIndicator)
+    }
+
+    //build figure w/ child elements
+    mySection.appendChild(myName)
+    mySection.appendChild(myRating)
+    mySection.appendChild(myReview)
+    
+    myTarget.appendChild(mySection)
+}
+
+
 function toggleMenu(){
 document.getElementById("primaryNav").classList.toggle("open")
+document.getElementById("hamburgerBtn").classList.toggle("open")
 }
 
 const hamburgerBtn = document.getElementById("hamburgerBtn")
